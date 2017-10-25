7eca7826eb66bf5303c813fc35590e3be35e068f
Justin Pratley <justinpratley@yahoo.com>
2017-10-24 23:24:04 -0400

    Updated Client Class

--- Eclipse/src/Client.java
+++ Eclipse/src/Client.java
@@ -1,8 +1,11 @@
+import java.util.*;
 public class Client{
+	private String name;
+	private String phoneNumber;
 	private String email;
 	private String address;
-	private Car[10] cars;
-	private WorkOrder[10] workOrders;
+	private Car[] cars = new Car[10];
+	private WorkOrder[] workOrders = new WorkOrder[10];
 
 	public Client(String name, String number, String email, String address){
 		this.name = name;
@@ -42,19 +45,34 @@ public String getAddress(){
 	return this.address;
 }
 
-public void addCar(/*New car constructor*/ car){
+public void addCar(){
+	int year;
+	Scanner scan = new Scanner(System.in);
+	System.out.print("Type: ");
+	String type = new String(scan.nextLine());
+	System.out.print("Make: ");
+	String make = new String(scan.nextLine());
+	System.out.print("Model: ");
+	String model = new String(scan.nextLine());
+	System.out.print("Year: ");
+	year = scan.nextInt();
+	System.out.print("\nLicense Plate Number: ");
+	String license = new String(scan.nextLine());
+	
+	Car car = new Car(type, make, model, year, license);
+	
 	int index = -1;
 	for(int i = 0; i < 10; i++){
-		if(cars[i] = null){
+		if(this.cars[i] == null){
 			index = i;
 			break;
 		}
 	}
 
-	if(index = -1)
-		System.out.println(“Error: Client has too many cars.”);
+	if(index == -1)
+		System.out.println("Error: Client has too many cars.");
 	else
-		cars[index] = car;
+		this.cars[index] = car;
 }
 
 public void removeCar(Car car){
@@ -64,26 +82,34 @@ public void removeCar(Car car){
 	}
 }
 
-public void addWorkOrder(/*New work order constructor*/ wo){
-	int index = -1;
-	for(int i = 0; i < 10; i++){
-		if(workOrders[i] = null){
-			index = i;
-			break;
-		}
+public void addWorkOrder(Car car){
+	int carIndex = -1;
+	for(int i =0; i < 10; i++){
+		if(cars[i].getLicense().equals(car.getLicense()))
+			carIndex = i;
 	}
-
-	if(index = -1)
-		System.out.println(“Error: Client has too many work orders.”);
-	else
-		workOrders[index] = wo;
+	if(carIndex == -1)
+		System.out.println("Error: Car not found.");
+	else{
+		int index = -1;
+		for(int i = 0; i < 10; i++){
+			if(workOrders[i] == null){
+				index = i;
+				break;
+			}
+		}
+	
+		if(index == -1)
+		System.out.println("Error: Client has too many work orders.");
+		else{
+			WorkOrder wo = new WorkOrder(this, car);
+			workOrders[index] = wo;
+		}	
+	}	
 }
 
-public void removeWorkOrder(WorkOrder wo){
-	for(int i = 0; i < 10; i++){
-		if(workOrders[i].getNumber() == wo.getNumber())
-			workOrders[i] = null;
-	}
+public void removeWorkOrder(WorkOrder wo, Mechanic m){
+	wo.removeWorkOrder(m);
 }
 
 public Car getCar(String license){
